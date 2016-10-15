6a56fc83e5c6c7ccd6c47b5596f63a3ffd40939c
Alex <alex.lardschneider@gmail.com>
2016-10-15 15:32:35 +0200

    Changed host url

--- app/src/main/java/com/mobility/android/data/network/Endpoint.java
+++ app/src/main/java/com/mobility/android/data/network/Endpoint.java
@@ -2,7 +2,7 @@ package com.mobility.android.data.network;
 
 public final class Endpoint {
 
-    public static final String API = "https://south-tyrol.appspot.com/";
+    public static final String API = "https://south-tyrol.appspot.com/mobility/";
     public static final String API_DATA = "https://sasa-bus-data.appspot.com/v1/";
 
     public static final String MAP_TILES = "map/coordinates/%d/%d/%d/line/%d/variant/%d";
