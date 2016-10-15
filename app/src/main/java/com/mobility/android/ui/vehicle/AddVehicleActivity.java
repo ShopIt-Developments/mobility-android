b669f1e6f8954ed0b8d05bd2d91db14236f86392
mfink9983 <martinfink99@gmail.com>
2016-10-15 19:58:29 +0200

    changed Addvehicle layout

--- app/src/main/java/com/mobility/android/ui/vehicle/AddVehicleActivity.java
+++ app/src/main/java/com/mobility/android/ui/vehicle/AddVehicleActivity.java
@@ -1,8 +1,13 @@
 package com.mobility.android.ui.vehicle;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.mobility.android.R;
 
@@ -12,6 +17,27 @@ import butterknife.ButterKnife;
 public class AddVehicleActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar) Toolbar toolbar;
+
+    @BindView(R.id.add_car_name_layout) TextInputLayout nameLayout;
+    @BindView(R.id.add_car_name) EditText name;
+
+    @BindView(R.id.radio_group_car_bicycle) RadioGroup radioGroup;
+    @BindView(R.id.radio_button_car) RadioButton radioCar;
+    @BindView(R.id.radio_button_bicycle) RadioButton radioBicycle;
+
+    @BindView(R.id.add_car_availability_layout) TextInputLayout availabilityLayout;
+    @BindView(R.id.add_car_availability) EditText availability;
+
+    @BindView(R.id.add_car_licence_plate_layout) TextInputLayout licencePlateLayout;
+    @BindView(R.id.add_car_licence_plate) EditText licencePlate;
+
+    @BindView(R.id.add_car_description_layout) TextInputLayout descriptionLayout;
+    @BindView(R.id.add_car_description) EditText description;
+
+    @BindView(R.id.add_car_price_layout) TextInputLayout priceLayout;
+    @BindView(R.id.add_car_price) EditText price;
+
+    @BindView(R.id.add_vehicle_accept) CardView accept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
